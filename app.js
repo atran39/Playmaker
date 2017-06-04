8dbca480008331924afff9aea2c4ffb9a345d543
atran39 <akt.tran@yahoo.com>
2017-06-03 22:27:33 -0400

    ajax request, error handling implemented

--- app.js
+++ app.js
@@ -2,26 +2,22 @@
 
 	var g_name = '';
 	var g_tracks = [];
-
-	var track_uris = [];
-
-	var g_access_token = '';
-	var g_username = '';
+	var track_urls = [];
 
 	var client_id = '4d78f4f5135944c9b2f05bcabff6b557';
-	var redirect_uri = 'http://localhost:8000/callback.html';
+	var redirect_uri = 'http://localhost/callback.html';
 
 	var splitText = function(inputtext) {
 		var words = inputtext
-			.split(/[\n\r]/)
-			// .map(function(w) { return w.replace(/^\d+ /,'').replace(/[\d,]+$/,'')}) // rm rank, count
-			.map(function(w) { return w.trim().replace(/^[.,-]+/,'').replace(/[.,-]+$/g,''); })
-			.filter(function(w) { return (w.length > 0); });
+		.split(/[\n\r]/)
+		// .map(function(w) { return w.replace(/^\d+ /,'').replace(/[\d,]+$/,'')}) // rm rank, count
+		.map(function(w) { return w.trim().replace(/^[.,-]+/,'').replace(/[.,-]+$/g,''); })
+		.filter(function(w) { return (w.length > 0); });
 		return words;
 	}
 
 	var addToQueue = function() {
-		var tracks = null;
+		var new_urls = [];
 
 		g_name = $('#plName').val().trim();
 		if (g_name.trim() == "") {
@@ -54,72 +50,93 @@
 			} else {
 				// CSV
 				url = 'https://api.spotify.com/v1/search?q='
-					+ encodeURIComponent(curr[0] + ' ' + curr[1]) + '&type=track&limit=1';
+				+ encodeURIComponent(curr[0] + ' ' + curr[1]) + '&type=track&limit=1';
 			}
 
-			track_uris.push(url);
-			$.ajax(url, {
-				dataType: 'json',
-				success: function(r) {
-					console.log('got track', r);
-				},
-				error: function(r) {
-					console.log(r);
-				}
-			});
-		});
+			new_urls.push(url);
+		});.
+
+		return track_urls = new_urls;
 	}
 
-	var createPl = function () {
+	var search = function () {
 		var txt = '';
-		addToQueue();
+		var new_tracks = [];
 
-		track_uris.forEach(function(url) {
-		//while (track_uris.length != 0) {
+		var accessToken = location.hash.match(/^\#access_token=(.+)&token_type=(.+)&expires_in=(\d+)$/)[1];
+
+		track_urls.forEach(function(url) {
+			//while (track_uris.length != 0) {
 
 			//url = track_uris[0];
 
-			console.log(url);
-			// $.ajax(url, {
-			// 	dataType: 'json',
-			// 	success: function(r) {
-			// 		// console.log('got track', r);
-			// 		track_uris.shift();
-			// 		if (jQuery.isEmptyObject(r.tracks.items)) {
-			// 			console.log('fail to get track info')
-			// 			// not found on spotify
-			// 			name = url.replace(/^https:\/\/api\.spotify\.com\/v1\/search\?q=/, '').replace(/&type=track&limit=1$/,'');
-			// 			txt += '<div class="media">No match found for "' + decodeURIComponent(name)+ '"</div>\n';
-			// 		} else {
-			// 			console.log('found track', r);
-			// 			g_tracks.push(r.tracks.items[0].uri);
-			// 			txt += '<div class="media">' +
-			// 			'<a class="pull-left" href="#"><img class="media-object" src="' + r.tracks.items[0].album.images[r.tracks.items[0].album.images.length - 1].url + '" /></a>' +
-			// 			'<div class="media-body">' +
-			// 			'<h4 class="media-heading"><a href="' + r.tracks.items[0].uri + '">' + r.tracks.items[0].name + '</a></h4>' +
-			// 			'Album: <a href="' + r.tracks.items[0].album_uri + '">' + r.tracks.items[0].album.name +
-			// 			'</a><br/>Artist: <a href="' + r.tracks.items[0].artists[0].uri + '">' + r.tracks.items[0].artists[0].name+'</a>' +
-			// 			'</div>' +
-			// 			'</div>\n';
-			// 		}
-			// 	},
-			// 	error: function(r) {
-			// 		// timeout
-			// 		console.log(r);
-			// 	}
-			// });
+			$.ajax({
+				url: url,
+				dataType: 'json',
+				headers : {
+					'Authorization': 'Bearer ' + accessToken
+				},
+				async: false,
+				success: function(r) {
+					// console.log('got track', r);
+					// track_uris.shift();
+					if (jQuery.isEmptyObject(r.tracks.items)) {
+						console.log('fail to get track info', r)
+						// not found on spotify
+						name = url.replace(/^https:\/\/api\.spotify\.com\/v1\/search\?q=/, '').replace(/&type=track&limit=1$/,'');
+						txt += '<div class="media">No match found for "' + decodeURIComponent(name)+ '"</div>\n';
+					} else {
+						// console.log('found track', r);
+						// console.log(r.tracks.items[0].uri);
+						new_tracks.push(r.tracks.items[0].uri);
+						txt += '<div class="media">' +
+						'<a class="pull-left" href="#"><img class="media-object" src="' + r.tracks.items[0].album.images[r.tracks.items[0].album.images.length - 1].url + '" /></a>' +
+						'<div class="media-body">' +
+						'<h4 class="media-heading"><a href="' + r.tracks.items[0].uri + '">' + r.tracks.items[0].name + '</a></h4>' +
+						'Album: <a href="' + r.tracks.items[0].album_uri + '">' + r.tracks.items[0].album.name +
+						'</a><br/>Artist: <a href="' + r.tracks.items[0].artists[0].uri + '">' + r.tracks.items[0].artists[0].name+'</a>' +
+						'</div>' +
+						'</div>\n';
+						$('#tracks').html(txt);
+					}
+				},
+				error: function(jqXHR,textStatus,errorThrown) {
+					// timeout
+					console.log('errorThrown:',errorThrown);
+					if (errorThrown == 'Unauthorized') {
+						window.location.replace('http://localhost/callback.html');
+					} else if (errorThrown == 'Too Many Requests') {
+						// time delay
+						console.log('too');
+					}
+				}
+			});
 
-			$('#debug').html(txt);
-		//}
+			//}
 		});
+
+		return new_tracks;
+	}
+	var createPl = function () {
+
+		addToQueue();
+		var g_tracks = search();
+
+		console.log('done searching');
+		g_tracks.forEach(function(it) {
+			console.log(it);
+		});
+		console.log('done');
+
+		// need to wait for ajax requests to finish before processing
 	}
 
 
 	var doLogin = function(callback) {
 		var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
-			'&response_type=token' +
-			'&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
-			'&redirect_uri=' + encodeURIComponent(redirect_uri);
+		'&response_type=token' +
+		'&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
+		'&redirect_uri=' + encodeURIComponent(redirect_uri);
 		localStorage.setItem('createplaylist-tracks', JSON.stringify(g_tracks));
 		localStorage.setItem('createplaylist-name', g_name);
 		var w = window.open(url, 'asdf', 'WIDTH=400,HEIGHT=500');
@@ -134,6 +151,6 @@
 			doLogin(function() {});
 		});
 
-}
+	}
 
 })(window);
